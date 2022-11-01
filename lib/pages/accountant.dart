6698b571641d404ca0940bab68bdff81cc0b6968
B6Infinity <broteendas.official@gmail.com>
2022-11-01 11:22:06 +0530

    createNode lookin good... // TODO: Now add the data to DB

--- lib/pages/accountant.dart
+++ lib/pages/accountant.dart
@@ -1,13 +1,19 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:moni/db/nodes_databse.dart';
 import 'package:moni/model/node.dart';
+import 'package:moni/utils/controllers.dart';
+import 'package:moni/utils/methods.dart';
 import 'package:moni/widgets/accountant_body.dart';
 
 class Accountant extends StatefulWidget {
@@ -72,34 +78,172 @@ class _AccountantState extends State<Accountant> {
     //       present_amt: 50),
     // );
 
+    Color nodeBackgroundColor = Colors.black;
+    Color nodeTextColor = Colors.blue;
+
     showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (context) {
-        return Dialog(
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Create Node...',
-                    style: TextStyle(
-                      fontSize: 25,
+        //
+        void changeBGColor(Color bgcolor) {
+          setState(() {
+            nodeBackgroundColor = bgcolor;
+          });
+        }
+
+        void changeTXTColor(Color bgcolor) {
+          setState(() {
+            nodeTextColor = bgcolor;
+          });
+        }
+
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Create Node...',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.green,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(hintText: 'Name'),
-                ),
-                TextField(
-                  decoration: InputDecoration(hintText: 'Color'),
-                ),
+                  SizedBox(height: 20),
+                  TextField(
+                    decoration: InputDecoration(label: Text('Name')),
+                    controller: node_inputcontroller__NAME,
+                  ),
+                  ListTile(
+                    contentPadding:
+                        EdgeInsets.only(left: 0, right: 8, top: 8, bottom: 8),
+                    title: Text('Background Color'),
+                    trailing: ElevatedButton(
+                      onPressed: (() {
+                        // pickColor();
+                        showDialog(
+                          context: context,
+                          barrierDismissible: false,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  ColorPicker(
+                                    pickerColor: nodeBackgroundColor,
+                                    onColorChanged: (value) =>
+                                        changeBGColor(value),
+                                  ),
+                                ],
+                              ),
+                              actions: [
+                                TextButton(
+                                    onPressed: (() {
+                                      setState(() {
+                                        Navigator.of(context).pop();
+                                      });
+                                    }),
+                                    child: Text('OK'))
+                              ],
+                            );
+                          },
+                        );
+                      }),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: nodeBackgroundColor,
+                      ),
+                      child: const Icon(Icons.color_lens),
+                    ),
+                  ),
+                  ListTile(
+                    contentPadding:
+                        EdgeInsets.only(left: 0, right: 8, top: 0, bottom: 8),
+                    title: Text('Text Color'),
+                    trailing: ElevatedButton(
+                      onPressed: (() {
+                        // pickColor();
+                        showDialog(
+                          context: context,
+                          barrierDismissible: false,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  ColorPicker(
+                                    pickerColor: nodeBackgroundColor,
+                                    onColorChanged: (value) =>
+                                        changeBGColor(value),
+                                  ),
+                                ],
+                              ),
+                              actions: [
+                                TextButton(
+                                    onPressed: (() {
+                                      setState(() {
+                                        Navigator.of(context).pop();
+                                      });
+                                    }),
+                                    child: Text('OK'))
+                              ],
+                            );
+                          },
+                        );
+                      }),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: nodeTextColor,
+                      ),
+                      child: const Icon(Icons.color_lens),
+                    ),
+                  ),
+                  TextField(
+                    controller: node_inputcontroller__TARGET_AMT,
+                    decoration: InputDecoration(label: Text('Target Amt.')),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      // GET THE VALUES:
+                      final name = node_inputcontroller__NAME.text;
+                      final maxAmt = node_inputcontroller__TARGET_AMT.text;
+                      final bgColor = nodeBackgroundColor;
+                      final textColor = nodeTextColor;
+
+                      // FRISK
+                      if (name.isEmpty) {
+                        showSnackBarMSG(context, 'Name is empty!');
+                        return;
+                      }
+                      if (maxAmt.isEmpty) {
+                        showSnackBarMSG(context, 'Max Amount is empty!');
+                        return;
+                      }
+
+                      print('$name $maxAmt $bgColor $textColor');
+
+                      // ADD 2 DB
+                    },
+                    child: const Text('CREATE')),
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      node_inputcontroller__NAME.text = '';
+                      node_inputcontroller__TARGET_AMT.text = '';
+                    },
+                    child: const Text('CANCEL')),
               ],
-            ),
-          ),
+            );
+          },
         );
       },
     );
---
+++ lib/utils/controllers.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/cupertino.dart';
+
+final node_inputcontroller__NAME = TextEditingController();
+final node_inputcontroller__TARGET_AMT = TextEditingController();
